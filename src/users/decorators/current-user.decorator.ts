14665128a7d625702de48072233a963629bc014f
Timilehin Arigbede <harriemannie@gmail.com>
2025-02-26 11:43:22 +0000

    Custom Param Decorator

---
+++ src/users/decorators/current-user.decorator.ts
@@ -0,0 +1,11 @@
+import { from } from "rxjs";
+import {
+  createParamDecorator,
+  ExecutionContext,
+} from "@nestjs/common";
+
+export const CurrentUser = createParamDecorator(
+  (data: any, context: ExecutionContext) => {
+    return 'hi there';
+  }
+)
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -16,6 +16,7 @@ import { UsersService } from './users.service';
 import { Serialize } from '../interceptors/serialize.interceptor';
 import { UserDto } from './dtos/user.dto';
 import { AuthService } from './auth.service';
+import { CurrentUser } from './decorators/current-user.decorator';
 
 
 @Controller('auth')
@@ -45,8 +46,8 @@ export class UsersController {
 
   // Get Current User
   @Get('/currentUser')
-  getCurrentUser(@Session() session: any) {
-    return this.usersService.findOne(session.userId);
+  getCurrentUser(@CurrentUser() user: string) {
+    return user;
   }
 
   // Sign Out
