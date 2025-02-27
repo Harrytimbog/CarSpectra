b58783888d4b98ed9a8337cff2b9e1b5b60e3fff
Timilehin Arigbede <harriemannie@gmail.com>
2025-02-27 20:37:09 +0000

    Unit Testinhg a Controller

--- src/users/users.controller.spec.ts
+++ src/users/users.controller.spec.ts
@@ -1,10 +1,29 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UsersController } from './users.controller';
+import { UsersService } from './users.service';
+import { AuthService } from './auth.service';
+import { User } from './user.entity';
+import { BadRequestException, NotFoundException } from '@nestjs/common';
 
 describe('UsersController', () => {
   let controller: UsersController;
+  let fakeUsersService: Partial<UsersService>;
+  let fakeAuthService: Partial<AuthService>;
 
   beforeEach(async () => {
+    fakeAuthService = {
+      findOne: () => { },
+      find: () => { },
+      remove: () => { },
+      update: () => { },
+    };
+
+    fakeUsersService = {
+      // fake implement
+      signup: () => { },
+      signin: () => { }
+    };
+
     const module: TestingModule = await Test.createTestingModule({
       controllers: [UsersController],
     }).compile();
