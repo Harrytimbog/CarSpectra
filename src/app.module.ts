63d71db5e4394d10a9fd6b479b527901f3ebcb81
Timilehin Arigbede <harriemannie@gmail.com>
2025-03-06 18:04:57 +0000

    Specifying the Runtown Environment | Untracking env files from git

--- src/app.module.ts
+++ src/app.module.ts
@@ -28,13 +28,13 @@ const cookieSession = require('cookie-session');
           synchronize: true,
         }
       },
-    })
-  //   TypeOrmModule.forRoot({
-  //   type: 'sqlite',
-  //   database: 'db.sqlite',
-  //   entities: [User, Report],
-  //   synchronize: true,
-  // }),
+    }),
+    //   TypeOrmModule.forRoot({
+    //   type: 'sqlite',
+    //   database: 'db.sqlite',
+    //   entities: [User, Report],
+    //   synchronize: true,
+    // }),
     ReportsModule,
     UsersModule],
   controllers: [AppController, UsersController],
