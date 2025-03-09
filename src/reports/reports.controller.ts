297861eaf23700d1ba46e8634efa515e88795691
Timilehin Arigbede <harriemannie@gmail.com>
2025-03-09 21:12:09 +0000

    Assigning CurrentUser with a Middleware instead of Interceptor | This is to avoid error with the user data is set to the request data early

--- src/reports/reports.controller.ts
+++ src/reports/reports.controller.ts
@@ -7,6 +7,7 @@ import { User } from 'src/users/user.entity';
 import { Serialize } from 'src/interceptors/serialize.interceptor';
 import { ReportDto } from './dtos/report.dto';
 import { ApproveReportDto } from './dtos/approve-report.dto';
+import { AdminGuard } from 'src/guards/admin.guard';
 
 @Controller('reports')
 export class ReportsController {
@@ -21,7 +22,7 @@ export class ReportsController {
 
   // Approve or unapprove a report by id (Admin only)
   @Patch('/:id')
-  @UseGuards(AuthGuard)
+  @UseGuards(AdminGuard)
   approveReport(@Param('id') id: string, @Body() body: ApproveReportDto) {
     return this.reportsService.changeApproval(id, body.approved);
   }
--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,23 +1,23 @@
-import { Module } from '@nestjs/common';
-import { APP_INTERCEPTOR } from '@nestjs/core';
+import { MiddlewareConsumer, Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './user.entity';
 import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
 import { AuthService } from './auth.service';
-import { CurrentUserInterceptor } from './interceptors/current-user.interceptor';
+import { CurrentUserMiddleware } from './midddlewares/current-user.middleware';
 
 @Module({
   imports: [TypeOrmModule.forFeature([User])],
   providers: [
     UsersService,
-    AuthService,
-    {
-      provide: APP_INTERCEPTOR,
-      useClass: CurrentUserInterceptor
-    },
+    AuthService
   ],
   controllers: [UsersController],
   exports: [UsersService, AuthService],
 })
-export class UsersModule { }
+export class UsersModule {
+  // Apply the CurrentUserMiddleware to all routes in our application
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(CurrentUserMiddleware).forRoutes('*');
+  }
+}
