8a94508ee9ff3c4adc69b172002483155b5347c6
Timilehin Arigbede <harriemannie@gmail.com>
2025-03-26 10:00:29 +0000

    Env-Specific Database Config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,15 +1,12 @@
 import { MiddlewareConsumer, Module, ValidationPipe } from '@nestjs/common';
-import { User } from './users/user.entity';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ReportsModule } from './reports/reports.module';
-import { UsersController } from './users/users.controller';
-import { UsersService } from './users/users.service';
 import { UsersModule } from './users/users.module';
-import { Report } from './reports/report.entity';
 import { APP_PIPE } from '@nestjs/core';
+import { AppDataSource } from './ormconfig'; // <-- important
 const cookieSession = require('cookie-session');
 
 @Module({
@@ -18,46 +15,32 @@ const cookieSession = require('cookie-session');
       isGlobal: true,
       envFilePath: `.env.${process.env.NODE_ENV}`,
     }),
-    TypeOrmModule.forRootAsync({
-      inject: [ConfigService],
-      useFactory: (config: ConfigService) => {
-        return {
-          type: 'sqlite',
-          database: config.get<string>('DB_NAME'),
-          entities: [User, Report],
-          synchronize: true,
-        }
-      },
-    }),
-    //   TypeOrmModule.forRoot({
-    //   type: 'sqlite',
-    //   database: 'db.sqlite',
-    //   entities: [User, Report],
-    //   synchronize: true,
-    // }),
+    // ✅ Provide connection options from AppDataSource
+    TypeOrmModule.forRoot(AppDataSource.options),
     ReportsModule,
-    UsersModule],
-  controllers: [AppController, UsersController],
+    UsersModule,
+  ],
+  controllers: [AppController],
   providers: [
     AppService,
     {
       provide: APP_PIPE,
-      // This is a global pipe that will be applied to all routes even e2e tests
       useValue: new ValidationPipe({
         whitelist: true,
       }),
     },
   ],
 })
-
 export class AppModule {
-  // This is a middleware that will be applied to all routes
   constructor(private configService: ConfigService) { }
+
   configure(consumer: MiddlewareConsumer) {
-    // consumer.apply(cookieSession
-    consumer.apply(cookieSession({
-      keys: [this.configService.get<string>('COOKIE_KEY')],
-    }),
-    ).forRoutes('*');
+    consumer
+      .apply(
+        cookieSession({
+          keys: [this.configService.get<string>('COOKIE_KEY')],
+        }),
+      )
+      .forRoutes('*');
   }
 }
